8c16d62655e8e62be27c10846d3049dd18469099
Martín Vargas <francisco.vargas@pucp.pe>
2023-04-26 15:17:32 -0500

    Automation Attempt 07

--- Opus/installerbot.sh
+++ Opus/installerbot.sh
@@ -21,6 +21,7 @@ if [ "$respuesta" == "ok Flaca" ]; then
     # Creamos algunos directorios que siempre usamos
     mkdir /home/mart/bin
     mkdir /home/mart/Documentos/R_Home
+    mkdir /home/mart/Documentos/Cahier
     mkdir /home/mart/Documentos/Cahier/Utilitarios
     mkdir /home/mart/Documentos/Acad
     mkdir /home/mart/Documentos/Trabajo
@@ -30,8 +31,31 @@ if [ "$respuesta" == "ok Flaca" ]; then
     mkdir /home/mart/Documentos/Trabajo/Teleperformance/2023
     
     # Instalamos
-    sudo apt install git jp2a curl vim ranger synaptic keepassxc speedtest-cli ncal vim-gtk mpv vlc terminator clementine calibre make yt-dlp
-      
+    sudo apt install jp2a 
+    sudo apt install curl 
+    sudo apt install vim 
+    sudo apt install ranger 
+    sudo apt install synaptic 
+    sudo apt install keepassxc
+    sudo apt install speedtest-cli 
+    sudo apt install ncal 
+    sudo apt install vim-gtk3
+    sudo apt install mpv 
+    sudo apt install vlc 
+    sudo apt install terminator 
+    sudo apt install clementine 
+    sudo apt install calibre 
+    sudo apt install make 
+    sudo apt install yt-dlp
+    sudo apt install fonts-powerline
+    sudo apt install ueberzug
+    
+    # Configuramos git y clonamos Vundle
+    git config --global user.email "francisco.vargas@pucp.pe"
+    git config --global user.name "Martín Vargas"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+    cd /home/mart/Documentos
+    git clone git@github.com:marvares/Nts.git /home/mart/Documentos/Nts
     
 else
     echo "Bueno, parece que te arrepentiste, o ejecutaste esto por error, o ya estás viejo y no te acuerdas de la frase, jajaja."
