1110c8d84bb4e925d79e08d0612e69ece452aefe
Martín Vargas <francisco.vargas@pucp.pe>
2023-05-03 00:55:50 -0500

    Añadí un paquete al Rbot.sh para instalar gt

--- Opus/Rbot.sh
+++ Opus/Rbot.sh
@@ -12,9 +12,10 @@
     # Coloca aquí las líneas de código para realizar la tarea que desees
     sudo apt install gfortran ;
     sudo apt-get install liblapack-dev -y ; sudo apt-get install liblapack3 -y ; sudo apt-get install libopenblas-base -y ; sudo apt-get install libopenblas-dev -y ;
-    sudo apt-get install libfontconfig1-dev ;
-    sudo apt-get install libxml2-dev ;
-    sudo apt install libcurl4-openssl-dev
+    sudo apt-get install libfontconfig1-dev -y ;
+    sudo apt-get install libxml2-dev -y ;
+    sudo apt install libcurl4-openssl-dev -y ;
+    sudo apt install libv8-dev -y ;
 # else
 #     echo "Bueno, parece que te arrepentiste, o ejecutaste esto por error, o ya estás viejo y no te acuerdas de la frase, jajaja."
 # fi
